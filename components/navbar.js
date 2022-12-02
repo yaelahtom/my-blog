089a365d722ed6aa8878a5965688cbc61f881411
santomi3112 <adiprasetio007@gmail.com>
2022-12-02 21:10:44 +0700

    sticky navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,3 +1,19 @@
+import Link from "next/link"
+
 export default function Navbar() {
-  return <div>ini navbar</div>
+  return (
+    <div className="bg-blue-600 max-w-screen-sm px-6 min-w-full fixed">
+      <nav className="flex justify-around">
+        <Link href="/" className="underline text-white">
+          Home
+        </Link>
+        <Link href="/" className="underline text-white">
+          About
+        </Link>
+        <Link href='/' className="underline text-white">
+          Projects
+        </Link>
+      </nav>
+    </div>
+  )
 }

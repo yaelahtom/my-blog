8127febf832f5e6e6004a172763c34159261fefb
santomi3112 <adiprasetio007@gmail.com>
2022-12-07 17:12:15 +0700

    removed unused cdn

--- pages/_document.js
+++ pages/_document.js
@@ -4,10 +4,6 @@ const MyDocument = () => {
   return (
     <Html lang='en'>
       <Head>
-        <link
-          rel='stylesheet'
-          href='https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/github-dark.min.css'
-        />
       </Head>
       <body>
         <Main />
